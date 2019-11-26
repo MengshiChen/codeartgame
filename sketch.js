9b42b267aeef58a0e1955412cc195ca70aff1a4b
MengshiChen <MengshiChen@users.noreply.github.com>
2019-11-25 16:41:04 -0800

    ...

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var xspeed = 1;
+var xpos = 0;
+var cloud;
+var sound;
+var grass;
+var img1;
+var img2;
+var img3;
+var img4;
+var img5;
+function preload() {
+  cloud=loadImage('clound-1.png');
+  sound=loadSound('mariogame.mp3');
+  grass = loadImage('grass-3.png');
+  img1= loadImage('background.PNG');
+  img2= loadImage('mouse-1.png');
+  img3= loadImage('sun1-1.png');
+  img4=loadImage('pikachu-1.png');
+  img5=loadImage('sun2-1.png');
+}
+
+function setup() {
+  createCanvas(1200,1200);
+  noCursor();
+  sound.play();
+}
+
+function draw() {
+
+  background(img1,0,0);
+  image(cloud, xpos, 300);
+  image(img2,mouseX,mouseY);
+  
+  if (xpos > width) {
+    
+    xspeed = -1;
+  }
+    
+  if (xpos< 0){
+    
+    xspeed = 1;
+  }
+    xpos += xspeed;
+    
+  if(mouseIsPressed){
+    image(img4,400,670);
+  }
+  else{image(grass,500,780);
+        }
+image(img2, mouseX,mouseY);
+
+  if(mouseIsPressed){
+    image(img5,20,20);
+  }
+  else{image(img3,20,20);
+        }
+image(img2, mouseX,mouseY);
+  
+}
+
